3886c583d234d133bd785c994bed324e963ce570
Marcos Vinicius Silva <marcoscv71@gmail.com>
2021-05-11 16:00:47 -0300

    Criação do projeto spring boot e configuração do banco

---
+++ src/test/java/com/gittub/marcoscv71/citiesapi/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gittub.marcoscv71.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
